b3a91aa5a31801f6ebaf4e76e3a78e2d56880f99
Ebo Turkson <mturkson23@gmail.com>
2022-05-24 01:12:47 +0000

    update time

--- src/app/components/utils/custom.timepipe.ts
+++ src/app/components/utils/custom.timepipe.ts
@@ -4,13 +4,10 @@ import { DatePipe } from '@angular/common';
 @Pipe({
   name: 'customTime'
 })
-export class CustomTimePipe extends DatePipe implements PipeTransform {
+
+export class CustomTimePipe extends 
+             DatePipe implements PipeTransform {
   override transform(value: any, args?: any): any {
-    // transformed values will be in minutes
-    let hours = (parseInt(value) / (60)).toFixed(1);
-    let days = (parseInt(value) / (60 * 24)).toFixed(1);
-    if (parseInt(value) < 60) return value + " mins";
-    else if (parseInt(hours) < 24) return hours + " h";
-    else return days + " Days";
+    return super.transform(value, "HH:mm");
   }
 }
